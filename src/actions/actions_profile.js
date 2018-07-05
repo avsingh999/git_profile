8af5e59450f300a25be866a80658fd3fb70ab0e1
avsingh999 <201651011@iiitvadodara.ac.in>
2018-07-05 17:17:39 +0530

    reademe

--- src/actions/actions_profile.js
+++ src/actions/actions_profile.js
@@ -4,7 +4,7 @@ export const PROFILE_EDITED = "PROFILE_EDITED";
 export function fetchProfile() {
     return (dispatch) => {
         let header = new Headers({ "Content-Type": "application/json", "Authorization": "token b2e2b495a3f6ec832ce44ab6a4924a30cca530a2" });
-        fetch("https://api.github.com/users/igniteeng000", {
+        fetch("https://api.github.com/users/avsingh999", {
             method: "GET",
             headers: header
         })
