27864a1d7ac2da3deec13df5d2dd182f5f951e93
Your Name <999.avsingh999@gmail.com>
2018-09-21 17:21:55 +0530

    update

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,5 @@
 import { connect } from 'react-redux';
 import { fetchProfile } from '../actions/actions_profile'
-
 import AppComponent from '../components/App';
 
 const mapDispatchToProps = (dispatch, ownProps) => {
@@ -10,10 +9,8 @@ const mapDispatchToProps = (dispatch, ownProps) => {
         }
     }
 }
-
 const App = connect(
     false,
     mapDispatchToProps
 )(AppComponent)
-
 export default App;
