89a173b2f1fa5bb83f641a04c81a02f19e48f7cb
Your Name <999.avsingh999@gmail.com>
2018-09-23 11:16:57 +0530

    add

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from "redux";
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
-
 import './index.css';
 import App from "./containers/App";
   
